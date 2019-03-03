aa43c89bacdd3572120361cc9c15098079f489ce
ayushi <am2862@njit.edu>
2019-03-03 15:31:53 -0500

    Feature: Reading record from CSV file

--- public/index.php
+++ public/index.php
@@ -10,15 +10,15 @@
  * Time: 2:29 PM
  */
 
-main::start("insurance.csv");
+main::start("example.csv");
 
 class main {
 
     static public function start($filename) {
 
         $records = csv::getRecords($filename);
-        $table = html::generateTable($records);
-        //system::printTable($table);
+        print_r($records);
+
     }
 
 }
@@ -47,6 +47,7 @@ class csv {
         }
 
         fclose($file);
+
         return $records;
     }
 }
@@ -80,92 +81,11 @@ class recordFactory{
 
     public static function create(Array $fieldNames = null, Array $record = null)
         {
-
             $record = new record($fieldNames,$record);
             return $record;
         }
 }
 
 
-class html {
 
-    static public function generateTable($records){
-
-        $count=0;
-
-
-        echo "<table class='table table-striped'>";
-
-        foreach($records as $record)
-        {
-
-
-               if ($count == 0) {
-                   $array = $record->returnArray();
-                   $keys = array_keys($array);
-                   $values = array_values($array);
-                   //print_r($keys);
-                   //print_r($values);
-                   //$table= array_combine($keys,$values);
-                   echo "<thead><tr>";
-                   foreach($keys as $key)
-                   echo "<th scope='col'>" . $key . "</th>";
-                   echo"</tr></thead><tbody><tr>";
-                   foreach($values as $value)
-                   echo "<td scope='row'>" . $value . "</td>";
-                   echo "</tr>";
-
-
-               } else {
-                   $array = $record->returnArray();
-                   $values = array_values($array);
-                   //print_r($values);
-                   //$table= array_merge($keys,$values);
-                   echo "<tr>";
-                   foreach($values as $value)
-                   echo "<td scope='row'>" . $value . "</td>";
-                   echo "</tr></tbody>";
-
-
-               }
-
-
-            $count++;
-
-        }
-
-        echo "</table>";
-
-    }
-}
-
-class system {
-
-    static public function printTable($table){
-
-        $count=0;
-
-        echo '<table class="table table-dark">';
-        foreach ($table as $row)
-        {
-            $arr=$row->returnArray();
-            $key=array_keys($arr);
-            $val=array_values($arr);
-            if ($count == 0)
-            {
-                echo"<thead><tr><th>".$key."</th></tr></thead>";
-            }
-            else
-            {
-                echo"<tbody><tr><td>$val</td></tr></tbody>";
-            }
-            $count++;
-        }
-        echo '</table><br />';
-
-
-       
-
-    }
-}
 
